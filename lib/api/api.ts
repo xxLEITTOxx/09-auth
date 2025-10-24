e66f9c6c0806956766420f055e0a86187b90e1bf
LEITTO <xxleittoxx@gmail.com>
2025-10-24 14:45:23 +0200

    api and env variables

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_API_URL
+  ? `${process.env.NEXT_PUBLIC_API_URL.replace(/\/$/, "")}/api`
+  : "/api";
+
 export const nextServer = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL,
   withCredentials: true,
 });
